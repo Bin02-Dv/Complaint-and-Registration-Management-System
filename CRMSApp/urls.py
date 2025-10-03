9fe524367b352a482b7511b310f4ee26c28e4786
Bin02-Dv <alaminbinahmad@gmail.com>
2025-10-03 14:47:38 -0700

    10/3/2025 - 2:47pm

--- CRMSApp/urls.py
+++ CRMSApp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signUp/", views.signUp, name="signUp"),
+    path("login/", views.login, name="login"),
+    
+    # user
+    path("user-dash/", views.user_dash, name="user-dash"),
+    path("file-complaint/", views.file_complaint, name="file-complaint")
 ]
--- CRMSApp/views.py
+++ CRMSApp/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def signUp(request):
+    return render(request, "signUp.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def user_dash(request):
+    return render(request, "USER/dash.html")
+
+def file_complaint(request):
+    return render(request, "USER/file-complaint.html")
+
